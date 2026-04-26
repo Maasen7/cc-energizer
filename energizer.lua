a95440f643fcca57e1929073f637985a931fa5da
Maasen7 <striker.1427@gmail.com>
2026-04-26 05:49:53 +0200

    Update energizer.lua

--- energizer.lua
+++ energizer.lua
@@ -1,10 +1,39 @@
 local e=peripheral.wrap("BigReactors-Energizer_0")
 local m=peripheral.wrap("left")
+
 m.setTextScale(0.5)
+
+local function center(y,text,color)
+ local w,_=m.getSize()
+ m.setCursorPos(math.floor((w-#text)/2)+1,y)
+ m.setTextColor(color or colors.white)
+ m.write(text)
+end
+
+local function bar(y,pct)
+ local w,_=m.getSize()
+ local bw=w-4
+ local fill=math.floor(bw*pct)
+
+ m.setCursorPos(3,y)
+
+ for i=1,bw do
+  if i<=fill then
+   if pct>0.6 then m.setBackgroundColor(colors.green)
+   elseif pct>0.3 then m.setBackgroundColor(colors.yellow)
+   else m.setBackgroundColor(colors.red) end
+  else
+   m.setBackgroundColor(colors.gray)
+  end
+  m.write(" ")
+ end
+
+ m.setBackgroundColor(colors.black)
+end
 
 while true do
  m.clear()
- local w,h=m.getSize()
+
  local en=e.getEnergyStoredAsText()
  local cap=e.getEnergyCapacity()
  local i=e.getEnergyInsertedLastTick()
@@ -14,43 +43,40 @@ while true do
  local num=tonumber(string.match(en,"%d+")) or 0
  local pct=num/cap
 
- m.setCursorPos(2,1)
- m.write("ENERGIZER CONTROL")
+ -- Title
+ center(1,"⚡ ENERGIZER ⚡",colors.cyan)
 
- m.setCursorPos(2,3)
- m.write("State: "..(on and "ON" or "OFF"))
+ -- Status
+ center(3,"Status: "..(on and "ONLINE" or "OFFLINE"))
 
- m.setCursorPos(2,5)
- m.write(en)
+ -- Big %
+ center(5,string.format("%.1f%%",pct*100),colors.white)
 
- m.setCursorPos(2,6)
- m.write(string.format("%.1f%%",pct*100))
+ -- Energy text
+ center(6,en)
 
- local bw=w-2
- local fill=math.floor(bw*pct)
+ -- Bar
+ bar(8,pct)
 
- m.setCursorPos(2,8)
- for i2=1,bw do
-  if i2<=fill then m.setBackgroundColor(colors.green)
-  else m.setBackgroundColor(colors.gray) end
-  m.write(" ")
- end
- m.setBackgroundColor(colors.black)
+ -- Rates
+ center(10,"IN  : "..i.." FE/t")
+ center(11,"OUT : "..o.." FE/t")
 
- m.setCursorPos(2,10)
- m.write("In: "..i)
+ -- Button
+ local w,h=m.getSize()
+ local bx=math.floor((w-18)/2)
+ local by=h-2
 
- m.setCursorPos(2,11)
- m.write("Out: "..o)
-
- m.setCursorPos(2,h-1)
+ m.setCursorPos(bx,by)
  m.setBackgroundColor(on and colors.red or colors.green)
- m.write(" TAP TO TOGGLE ")
+ m.setTextColor(colors.white)
+ m.write("   TAP TO TOGGLE   ")
  m.setBackgroundColor(colors.black)
 
  local t=os.startTimer(0.5)
+
  while true do
-  local ev,a,x,y=os.pullEvent()
+  local ev,_,x,y=os.pullEvent()
   if ev=="timer" then break end
   if ev=="monitor_touch" then
    e.setActive(not on)
